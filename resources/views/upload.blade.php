c297cd16b8b93f4fcdf4f338745e9a6996167e24
Gaby-Rousse <gabrielpimpare23@gmail.com>
2026-03-02 16:23:58 +0000

    maerci ckoudflare

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,5 +1,5 @@
 @include('partials.header')
-<script src="scripts/musicUpload.js?v=3.5.5"></script>
+<script src="scripts/musicUpload.js?v=3.5.9"></script>
 
 <input type="hidden" id="approvedMaxPages" value="{{$approvedMaxPages}}">
 <input type="hidden" id="pendingMaxPages" value="{{$pendingMaxPages}}">
