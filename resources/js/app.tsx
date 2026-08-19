704983900eaba3a0fdcdde234d451d78f6b2e325
Gabrielle Rousse <134748205+Gaby-Rousse@users.noreply.github.com>
2026-08-19 09:45:41 -0400

    Initial files

---
+++ resources/js/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
+
+createRoot(document.getElementById('root')!).render(
+    <RouterProvider router={router} />
+);
---
+++ resources/views/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.tsx'])
+</head>
+<body><div id="root"></div></body>
+</html>
